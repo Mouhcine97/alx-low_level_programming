1937d958da8498f17d97b28e6f685727ff0b8660
Mouhcine97 <mouhssine1997@outlook.fr>
2023-06-18 20:35:23 -0700

    c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - Project entry point
+*Return: 0
+*Description: 'printing the alphabets'
+*/
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+{
+putchar(n);
+n++;
+}
+putchar('\n');
+return (0);
+}
