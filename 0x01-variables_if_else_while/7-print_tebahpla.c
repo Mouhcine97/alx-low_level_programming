133bf764c5d7099f185b43057b5dca1e393ef90d
Mouhcine97 <mouhssine1997@outlook.fr>
2023-06-19 19:57:53 -0700

    c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - program entry point
+*Return: 0
+*Description: 'alphabets in reverse'
+*/
+int main(void)
+{
+	int n = 122;
+
+	while (n >= 97)
+	{
+	putchar(n);
+	n--;
+	}
+	putchar('\n');
+	return (0);
+
+}
+
