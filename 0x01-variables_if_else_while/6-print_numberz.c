ec4d9c53b680111eae19e3e373a131ea49d67a12
Mouhcine97 <mouhssine1997@outlook.fr>
2023-06-19 19:44:53 -0700

    c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - program entry point
+*Return: 0
+*Description: 'print numbers from 0 to 1'
+*/
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+	putchar(n);
+	n++;
+	}
+	putchar('\n');
+	return (0);
+}
