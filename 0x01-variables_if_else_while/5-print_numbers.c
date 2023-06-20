df3a1e4afd045ee6f34aeb0a1c54916c187d7367
Mouhcine97 <mouhssine1997@outlook.fr>
2023-06-19 19:15:13 -0700

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - program entry point
+*Return: 0
+*Description: 'print all alphabets exept e and q'
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
